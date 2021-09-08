f015d99b795c896434250fc9e4e5e10b6e2c7872
Dandrea Omar <omar_dandrea97@hotmail.com>
2021-09-07 23:57:54 -0300

    cambio de consulta get en Detalle Reclamo

--- Controllers/DetalleReclamoController.cs
+++ Controllers/DetalleReclamoController.cs
@@ -27,23 +27,30 @@ namespace ApiRVM2019.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-
             var _DetReclamo = from DetalleReclamo in context.DetalleReclamo
-                              join Reclamo in context.Reclamo on DetalleReclamo.ID_Reclamo equals Reclamo.IDReclamo
-                              join Sesion in context.Sesion on Reclamo.ID_Sesion equals Sesion.IDSesion
-                              join Usuario in context.Usuario on Sesion.ID_Usuario equals Usuario.IDUsuario
-                              join TipoReclamo in context.TipoReclamo on Reclamo.ID_TipoReclamo equals TipoReclamo.IDTipoReclamo
-                              join Estado in context.Estado on Reclamo.ID_Estado equals Estado.IDEstado
+                              join reclamo in context.Reclamo on DetalleReclamo.ID_Reclamo equals reclamo.IDReclamo
+                              join estado in context.Estado on reclamo.ID_Estado equals estado.IDEstado
+                              join tipoestado in context.TipoEstado on estado.ID_TipoEstado equals tipoestado.IDTipoEstado
+                              join sesion in context.Sesion on reclamo.ID_Sesion equals sesion.IDSesion
+                              join usuario in context.Usuario on sesion.ID_Usuario equals usuario.IDUsuario
                               select new
                               {
-                                  TipoReclamo = TipoReclamo.Nombre,
-                                  Descripcion = DetalleReclamo.Descripcion,
-                                  Fecha = Reclamo.Fecha,
-                                  Hora = Reclamo.Hora,
-                                  Foto = Reclamo.Foto,
-                                  Estado = Estado.Nombre,
-                                  Direccion = DetalleReclamo.Direccion
+
+                                  IDDetReclamo = DetalleReclamo.IDDetalleReclamo,
+                                  DescripcionDR = DetalleReclamo.Descripcion,
+                                  AlturaR = DetalleReclamo.Altura,
+                                  DireccionR = DetalleReclamo.Direccion,
+                                  ID_reclamo = DetalleReclamo.ID_Reclamo,
+                                  fechaR = reclamo.Fecha,
+                                  horaR = reclamo.Hora,
+                                  estadoR = estado.Nombre,
+                                  estadoid = estado.IDEstado,
+                                  estadoT = tipoestado.nombre,
+                                  estadoTid = tipoestado.IDTipoEstado,
+                                  usuarioR = usuario.Nick,
+
                               };
+           
 
             if (_DetReclamo == null)
             {
@@ -56,7 +63,23 @@ namespace ApiRVM2019.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<DetalleReclamo>> GetDetalleReclamo(int id)
         {
-            var _DetReclamo = await context.DetalleReclamo.FindAsync(id);
+            var _DetReclamo = await context.DetalleReclamo.FindAsync(from DetalleReclamo in context.DetalleReclamo
+                                                                     join Reclamo in context.Reclamo on DetalleReclamo.ID_Reclamo equals Reclamo.IDReclamo
+                                                                     join Sesion in context.Sesion on Reclamo.ID_Sesion equals Sesion.IDSesion
+                                                                     join Usuario in context.Usuario on Sesion.ID_Usuario equals Usuario.IDUsuario
+                                                                     join TipoReclamo in context.TipoReclamo on Reclamo.ID_TipoReclamo equals TipoReclamo.IDTipoReclamo
+                                                                     join Estado in context.Estado on Reclamo.ID_Estado equals Estado.IDEstado
+                                                                     where Usuario.IDUsuario == id
+                                                                     select new
+                                                                     {
+                                                                         TipoReclamo = TipoReclamo.Nombre,
+                                                                         Descripcion = DetalleReclamo.Descripcion,
+                                                                         Fecha = Reclamo.Fecha,
+                                                                         Hora = Reclamo.Hora,
+                                                                         Foto = Reclamo.Foto,
+                                                                         Estado = Estado.Nombre,
+                                                                         Direccion = DetalleReclamo.Direccion
+                                                                     });
 
             if (_DetReclamo == null)
             {
