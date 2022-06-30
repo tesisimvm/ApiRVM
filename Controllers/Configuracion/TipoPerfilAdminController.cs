3ddd64aab1bbe35a0b0bd46d4e8707ae355c56b1
Dandrea Omar <omar_dandrea97@hotmail.com>
2022-06-30 12:11:18 -0300

    nuevos controladores tipo perfil y tipo reclamo para la configuracion

---
+++ Controllers/Configuracion/TipoPerfilAdminController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiRVM2019.Contexts;
+using ApiRVM2019.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiRVM2019.Controllers.Configuracion
+{
+    [EnableCors("All")]
+    [Route("[controller]")]
+    [ApiController]
+    public class TipoPerfilAdminController : ControllerBase
+    {
+        private readonly AppDbContext context;
+
+        public TipoPerfilAdminController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/<TipoPerfilAdminController>
+        [HttpGet]
+        public IActionResult getPerfiles() 
+        {
+
+            var data = from Perfil in context.Perfil
+                       select new
+                       {
+                           idPerfil = Perfil.IDPerfil,
+                           nombre = Perfil.Nombre,
+                       };
+            if (data == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(data);
+        }
+
+        // GET api/<TipoPerfilAdminController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<TipoPerfilAdminController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<TipoPerfilAdminController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TipoPerfilAdminController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Controllers/Configuracion/TipoReclamoAdminController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiRVM2019.Contexts;
+using ApiRVM2019.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiRVM2019.Controllers.Configuracion
+{
+    [EnableCors("All")]
+    [Route("[controller]")]
+    [ApiController]
+    public class TipoReclamoAdminController : ControllerBase
+    {
+        private readonly AppDbContext context;
+
+        public TipoReclamoAdminController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/<TipoReclamoAdminController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<TipoReclamoAdminController>/5
+        [HttpGet("{idTipoReclamo}")]
+        public IActionResult GetTipoR(int idTipoReclamo)
+        {
+            var data = from TipoReclamo in context.TipoReclamo
+                       where TipoReclamo.IDTipoReclamo == idTipoReclamo
+                       select new
+                       {
+                           idTipoR = TipoReclamo.IDTipoReclamo,
+                           nombre = TipoReclamo.Nombre,
+                           descripcion = TipoReclamo.Descripcion,
+                       };
+            if (data == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(data);
+        }
+
+        // POST api/<TipoReclamoAdminController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<TipoReclamoAdminController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TipoReclamoAdminController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
